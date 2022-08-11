bfaea8174a6a90e7891ac3de2276233aba15f2cf
foziljon002 <f.eshmurodov@uic.group>
2022-08-11 13:12:41 +0500

    add pages and react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import Icon from 'components/common/Icon';
+import AppRouter from 'routes/AppRouter';
 import './static/styles/main.scss';
 
 function App() {
   return (
     <div className="">
-      <Icon name="google" />
-      <h1>Start Coding...</h1>
+      <AppRouter />
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Icon from 'components/common/Icon';
+
+const Home = () => {
+  return (
+    <div>
+      <Icon name="google" />
+      <h1>Start Coding...</h1>
+    </div>
+  );
+};
+
+export default Home;
