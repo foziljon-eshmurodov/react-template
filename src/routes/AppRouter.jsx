09dd7aa183aff3b2981801f18125af1c985c599c
foziljon002 <f.eshmurodov@uic.group>
2022-08-11 14:07:26 +0500

    routes setup and scss variables

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route, Link } from 'react-router-dom';
-import Home from 'pages/Home.jsx';
+import ROUTES from './routes';
 
 const MainRouter = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  );
+  const renderRoutes = ROUTES.map(({ path, component }) => {
+    const TopLevelComponet = component;
+    return <Route key={path} path={path} element={<TopLevelComponet />} />;
+  });
+
+  return <Routes>{renderRoutes}</Routes>;
 };
 
 export default MainRouter;
